97b3b559d1d0364ecdee275251df18f5170326be
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-06-04 22:43:31 +0530

    add current endpoints and make new changes to settings.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def gerRoutes (request):
+    routes = [
+        "api/token/",
+        "api/token/refresh/",
+    ]
+    return JsonResponse(routes, safe=False)
